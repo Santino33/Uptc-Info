1e1f9f49bf0c93fa84227507f466569ed930c61c
Santino33 <davidnaranjo330@gmail.com>
2023-11-01 11:10:36 -0500

    Ventana fomrmulario estudiante corregida

--- src/Vista/VentanaFormularioEstudiante.java
+++ src/Vista/VentanaFormularioEstudiante.java
@@ -29,23 +29,27 @@ public class VentanaFormularioEstudiante extends JFrame {
         panelCampos = new JPanel();
         panelCampos.setLayout(new GridLayout(8, 2));
         tipografia = "Dialog";
-        lblCampo = new JLabel();
-        lblCampo.setFont(new Font(tipografia, Font.PLAIN, 12));
 
         for (int i = 0; i < 8; i++){
-            switch (i){
-                case 0: lblCampo.setText("Cedula :");
-                case 1: lblCampo.setText("Contraseña :");
-                case 2: lblCampo.setText("Nombre :");
-                case 3: lblCampo.setText("Apellido :");
-                case 4: lblCampo.setText("Semestre :");
-                case 5: lblCampo.setText("Edad :");
-                case 6: lblCampo.setText("Genero :");
-                case 7: lblCampo.setText("Materias en curso :");
-            }
             input = new InputText();
+            lblCampo = new JLabel();
+            lblCampo.setFont(new Font(tipografia, Font.PLAIN, 12));
+            switch (i){
+                case 0 -> lblCampo.setText("Cedula :");
+                case 1 -> lblCampo.setText("Contraseña :");
+                case 2 -> lblCampo.setText("Nombre :");
+                case 3 -> lblCampo.setText("Apellido :");
+                case 4 -> lblCampo.setText("Semestre :");
+                case 5 -> lblCampo.setText("Edad :");
+                case 6 -> lblCampo.setText("Genero :");
+                case 7 -> lblCampo.setText("Materias en curso :");
+            }
+
             panelCampos.add(lblCampo);
             panelCampos.add(input);
+            pack();
+            this.setVisible(true);
+            repaint();
         }
 
         //Panel boton
@@ -57,6 +61,8 @@ public class VentanaFormularioEstudiante extends JFrame {
         this.add(panelCampos, BorderLayout.CENTER);
         this.add(panelBoton, BorderLayout.SOUTH);
 
+        pack();
+        this.setVisible(true);
     }
 
     public JButton getBoton() {
